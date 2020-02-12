c0664e9d9b6fe2466a683570595f84c887baff6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-12 15:49:54 +0000

    Deploy christos97/softeng-ntua to github.com/christos97/softeng-ntua.git:gh-pages

--- back-end/auth/admin_auth.js
+++ back-end/auth/admin_auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const credentials = require('../config/credentials')
 
+
 module.exports = (req, res, next) => {
     try {
         const token =  req.headers['x-observatory-auth'];
