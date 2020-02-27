04b83e7a52f7472c53d899a2a13613a1fbad573e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 01:16:16 +0000

    Deploy christos97/softeng-ntua to github.com/christos97/softeng-ntua.git:gh-pages

--- generateHTML.js
+++ generateHTML.js
@@ -1,6 +1,6 @@
 const marked = require ('marked')
 var fs = require ('fs')
 
-var readStream = fs.readFileSync('README.md','utf-8')
+var readStream = fs.readFileSync('/home/xsrm/Desktop/softeng-ntua-master/energy_group012/README.md','utf-8')
 var markdown = marked (readStream)
-fs.writeFileSync('index.html', markdown)
+fs.writeFileSync('energy_group012/cli.html', markdown)
