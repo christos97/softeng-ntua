2a0b6326b2974514e42d1caea24114f045e6bea8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 14:21:11 +0000

    Deploy christos97/softeng-ntua to github.com/christos97/softeng-ntua.git:gh-pages

--- automate.sh
+++ automate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo 'Installing Dependencies'
 #sudo npm install -g --save oclif
-cd back-end && sudo npm install --save && sudo npm i && cd ..
-echo 'Running Tests for development on port 5000'
+cd back-end && sudo npm install --save --loglevel=error && sudo npm i --loglevel=error && cd ..
+echo 'Running backend tests for development stage on port 5000'
 echo 'Connecting to energyTest database'
 cd  back-end && npm run test --silent && cd ..
-cd energy_group012 && sudo npm install --save && sudo npm i && sudo npm link && cd .. && cd back-end
-echo 'Starting server for production on port 8765'
+cd energy_group012 && sudo npm install --save --loglevel=error && sudo npm i --loglevel=error && sudo npm link && cd .. && cd back-end
+echo 'Starting server for production stage on port 8765'
 NODE_ENV='production' npm start --silent 
 
